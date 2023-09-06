04391af8b87c68d1fd516d4300c5d6d0f963c447
serenitysmith <serenitysmith24@gmail.com>
2023-09-05 21:31:09 -0400

    youtube vid pracdtice color changing wiht react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import {useState, useEffect} from 'react'
+
+// changing thhe background color of body wit ract 
+function App(){
+  // setting the use state hook, to change the back ground olor of the page 
+  const [color, setColor] = useState("blue")
+  /// function for button to be funtional 
+  const click = color  => {
+    // this below doesnt change the color of the backgrond yet, just sets the variable color
+
+
+setColor(color)
+  }
+
+    // below is the use effect hook that will actually chnage the color 
+    useEffect(() => {
+      // below is what acually changes the background 
+    document.body.style.backgroundColor = color
+      // below in brakcets is the color array varaible in whats called a dependacy array 
+      // makes sure use effect is called every time the color changes
+    
+    }, [color])
+  return(
+    <div className="'App">
+{/* // we want to change it on click so we create a button  */}
+{/* // onclick is the event handler  */}
+<button onClick={
+  // pasing t eevent we want to happen on click
+  () => click("yellow")
+}>Change color</button>
+<div> how to change backgrond color with react </div>
+
     </div>
-  );
+  )
 }
-
 export default App;
